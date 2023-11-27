6cd2078b6b4bcd9b92fa5005e3732c1df248b559
Anderson Batista <andersonbn@outlook.com>
2023-11-27 15:57:00 -0300

    Aula sobre orientação e movimento de entidades

--- Warship/code/Game.py
+++ Warship/code/Game.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+import sys
+
 import pygame as pygame
 import self
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.Menu import Menu
+from code.level import Level
 
 
 class Game:
@@ -17,7 +20,14 @@ class Game:
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            else:
+                pygame.quit()
+                sys.exit()
 
 
 
